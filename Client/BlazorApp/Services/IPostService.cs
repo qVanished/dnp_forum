d2618884ba6bb72e73df7a5d1b5339fc805a13fb
qVanished <285800@via.dk>
2025-11-13 15:29:25 +0100

    added dependency injection for http services

--- Client/BlazorApp/Services/IPostService.cs
+++ Client/BlazorApp/Services/IPostService.cs
@@ -9,6 +9,7 @@ public interface IPostService
     public Task<CreatePostDTO> AddPostAsync(CreatePostDTO request);
     public Task UpdatePostAsync(int id, UpdatePostDTO request);
     public Task<Post> GetPostByIdAsync(int id);
+    public Task<List<Post>> GetPostsAsync();
     public Task<List<Post>> GetPostsAsync(int? userId, string? title);
     public Task DeletePostAsync(int id);
 }
