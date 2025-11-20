207fed80bb6efe259b910e8851bd331873cddaad
pimbp <pimbp@outlook.com>
2025-11-20 11:51:42 +0100

    db dependencies and class library created

---
+++ Server/EfcRepositories/AppContext.cs
@@ -0,0 +1,17 @@
+using System;
+using ClassLibrary1;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfcRepositories;
+
+public class AppContext : DbContext
+{
+    public DbSet<Post> Posts => Set<Post>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Comment> Comments => Set<Comment>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=app.db");
+    }
+}
